e4d070c740c05313ea0c2cc5da1b008dbdcbc24d
KentD <kentd600@gmail.com>
2025-11-19 10:14:54 +0900

    Changed autocomplete endpoint to post

--- backend/src/routes/Location.ts
+++ backend/src/routes/Location.ts
@@ -4,7 +4,7 @@ import { locationController } from '../c/rest/LocationController.js';
 
 const locationRouter = express.Router();
 
-locationRouter.get('/autocomp', rateLimits.autocomplete, async (req, res) => {
+locationRouter.post('/autocomp', rateLimits.autocomplete, async (req, res) => {
   const result = locationController.autocomplete(req);
   res.status(200).json(result);
 })
