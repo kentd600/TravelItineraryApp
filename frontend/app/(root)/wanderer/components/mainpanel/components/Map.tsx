19fced8d496d7be598a0aed1c08f70fdcaedcc7a
KentD <kentd600@gmail.com>
2025-11-19 11:37:16 +0900

    Updated map style

--- frontend/app/(root)/wanderer/components/mainpanel/components/Map.tsx
+++ frontend/app/(root)/wanderer/components/mainpanel/components/Map.tsx
@@ -74,7 +74,7 @@ export default function WandererMap () {
     <>
       <RMap
         id='wanderer-map'
-        mapStyle="https://tiles.stadiamaps.com/data/openmaptiles.json"
+        mapStyle="https://tiles.stadiamaps.com/styles/osm_bright.json"
         onLoad={(evt) => evt.target.setProjection({ type: 'globe' })}
         minZoom={1.5}
       >
