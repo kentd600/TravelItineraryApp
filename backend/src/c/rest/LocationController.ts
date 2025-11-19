85d5823c645928b16be6dbe3c920273404181800
KentD <kentd600@gmail.com>
2025-11-19 10:09:15 +0900

    Added autocomplete endpoint to backend

--- backend/src/c/rest/LocationController.ts
+++ backend/src/c/rest/LocationController.ts
@@ -1,7 +1,21 @@
 import { type Request } from 'express';
+import { LayerId, type FeaturePropertiesV2, GeocodingApi, Configuration } from '@stadiamaps/api';
+
+const config = new Configuration({ apiKey: process.env.STADIA_API_KEY! });
+const geoApi = new GeocodingApi(config);
+
+const layerStates = {
+  default: ['locality', 'country'] as LayerId[]
+}
 
 export const locationController = {
   async autocomplete(req: Request) {
-    
+    const { text } = req.body;
+    const result = await geoApi.autocompleteV2({
+      text,
+      lang: "en",
+      layers: layerStates.default
+    })
+    return result;
   }
 }
--- backend/src/routes/Location.ts
+++ backend/src/routes/Location.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import rateLimits from '../middleware/RateLimiter.js';
+import { locationController } from '../c/rest/LocationController.js';
 
 const locationRouter = express.Router();
 
-locationRouter.get('/autocomp', rateLimits.autocomplete, (req, res) => {
-  
+locationRouter.get('/autocomp', rateLimits.autocomplete, async (req, res) => {
+  const result = locationController.autocomplete(req);
+  res.status(200).json(result);
 })
 
 locationRouter.use(rateLimits.default);
