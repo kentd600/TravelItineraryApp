6d1fcadcc2c1c21dd0f54f6d3968c10f52058dc9
KentD <kentd600@gmail.com>
2025-11-29 15:37:19 +0900

    include credentials in auth client init

--- frontend/app/_utility/auth-client.ts
+++ frontend/app/_utility/auth-client.ts
@@ -1,7 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_WANDERER_API
+  baseURL: process.env.NEXT_PUBLIC_WANDERER_API,
+  fetchOptions: {
+    credentials: 'include'
+  }
 })
 
 export type Session = typeof authClient.$Infer.Session;
