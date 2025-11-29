d073ea45054925a124503482554e47a31fa1578e
KentD <kentd600@gmail.com>
2025-11-29 15:51:17 +0900

    Commented out allowed headers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,7 +22,7 @@ app.use(cors({
   origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  //allowedHeaders: ['Content-Type', 'Authorization'],
   preflightContinue: true
 }));
 
